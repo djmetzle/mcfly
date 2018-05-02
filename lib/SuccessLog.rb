33bb8ad0d050c91a5311837492ffd8a898cac6c3
Daryl Metzler <djmetzle@ncsu.edu>
2018-05-02 14:44:16 -0700

    Update SuccessLog (and tests)
    Make successlog allow finding specific entries.
    
    Also (thank you testing!) this now actually seeks around through
    the successlog to make reads and appends work correctly.

--- lib/SuccessLog.rb
+++ lib/SuccessLog.rb
@@ -8,16 +8,34 @@ class SuccessLog
    end
 
    def next_line
-      return @success_log_file.gets.chomp
+      line = @success_log_file.gets
+      return line ? line.chomp : nil
    end
 
    def at_end_of_success_log?
       return @success_log_file.eof?
    end
 
+   def entry_marked? entry
+      current_read_pos = @success_log_file.pos
+      found = false
+
+      @success_log_file.rewind
+      match = @success_log_file.each_line.lazy.find { |line|
+         line.chomp.eql? entry
+      }
+      if match
+         found = true
+      end
+
+      @success_log_file.pos = current_read_pos
+      return found
+   end
+
    def append entry
       current_read_pos = @success_log_file.pos
-      @success_log_file.puts entry.chomp
+      @success_log_file.seek(0, IO::SEEK_END)
+      @success_log_file.puts "#{entry.chomp}\n"
       @success_log_file.pos = current_read_pos
    end
 
--- spec/SuccessLog_spec.rb
+++ spec/SuccessLog_spec.rb
@@ -25,15 +25,44 @@ describe "SucessLog" do
             end
          end
       end
-   end
+      context "opens existing log file" do
+         it "opens existing log" do
+            log_file = File.open(@success_log_path, "w+")
+            log_file.puts "test string"
+            log_file.close
 
+            config = McFlyConfig.new :stream_path, @log_directory
+            @success_log = SuccessLog.new config
+
+            expect(@success_log.next_line).to eql "test string"
+         end
+
+      end
+   end
 
    describe "#next_line" do
       it "reads entries linearly" do
          @success_log.append("an entry")
          @success_log.append("another entry")
+         expect(@success_log.next_line).to eql "an entry"
+         expect(@success_log.next_line).to eql "another entry"
       end
    end
+
+   describe "#enrty_marked?" do
+      it "finds matching entries" do
+         @success_log.append("1 entry")
+         @success_log.append("2 entry")
+         @success_log.append("3 entry")
+
+         expect(@success_log.entry_marked? "1 entry").to be_truthy
+         expect(@success_log.entry_marked? "2 entry").to be_truthy
+         expect(@success_log.entry_marked? "3 entry").to be_truthy
+
+         expect(@success_log.entry_marked? "blue entry").to be_falsey
+      end
+   end
+
    describe "#append" do
       it "does not raise error" do
          expect { @success_log.append("an entry") }.not_to raise_error

e1243ea834f01efb63e019aa6460ec5ce6039a28
Daryl Metzler <daryl@ifixit.com>
2018-05-08 12:44:00 -0700

    Entry Work

--- lib/Entry.rb
+++ lib/Entry.rb
@@ -1,10 +1,18 @@
 class Entry
+   attr_accessor :entry_str
+   attr_accessor :entry_key
+   attr_accessor :entry_pool
+   attr_accessor :destinatino
+   attr_accessor :mcrouter_port
+
    def initialize(entry_str: entry_str,
                   entry_key: entry_key,
                   entry_pool: entry_pool,
                   destination: destination,
                   mcrouter_port: mcrouter_port
                  )
+      @entry_str = entry_str
+      @entry_key = entry_key
 
    end
 end

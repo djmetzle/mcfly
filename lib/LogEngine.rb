af2b18546df9c9d9a7f3fb8d2128fc673261da1c
Daryl Metzler <djmetzle@ncsu.edu>
2018-05-08 13:22:42 -0700

    Implement Queuing the parsed deletelog

--- lib/LogEngine.rb
+++ lib/LogEngine.rb
@@ -2,6 +2,7 @@ require 'McFlyConfig'
 require 'DeleteQueue'
 require 'SuccessLog'
 require 'DeleteLog'
+require 'EntryParser'
 
 class LogEngine
    def initialize config, delete_queue
@@ -10,14 +11,25 @@ class LogEngine
 
       @success_log = SuccessLog.new config
       @delete_log = DeleteLog.new config
+
+      @parser = EntryParser.new
    end
 
    def queue_delete_stream
+      if @delete_log.at_end_of_delete_log?
+         return false
+      end
 
+      while next_line = @delete_log.next_line
+         next_entry = @parser.parse next_line
+         break unless next_entry
+         @delete_queue.push next_entry.destination next_entry
+      end
+
+      return true
    end
 
    def mark_success entry
-
-
+      @success_log.append entry
    end
 end

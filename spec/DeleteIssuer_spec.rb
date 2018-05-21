ee026c651be99630ba91f87aee1ccc514a26348e
Daryl Metzler <djmetzle@ncsu.edu>
2018-05-21 11:50:42 -0700

    Flesh out Issuer tests
    Add a skeleton for testing the DeleteIssuer.
    
    The goal here is to inject the connector class. This way we can
    build a test connector to sanity check the behavior of the system.
    
    The "real" connector will perform the stateful memcached bits.

--- spec/DeleteIssuer_spec.rb
+++ spec/DeleteIssuer_spec.rb
@@ -1,5 +1,16 @@
 require './lib/DeleteIssuer'
 
-describe "DeleteIssuer" do
+TEST_CONNECTOR = "TestMemcachedConnector"
 
+describe "DeleteIssuer" do
+   before(:each) do
+      @queue = DeleteQueue.new
+      @issuer = DeleteIssuer.new @queue, TEST_CONNECTOR
+   end
+
+   describe ".flush_queue" do
+      it "succeeds when the queue is empty" do
+         expect { @issuer.flush_queue }.not_to raise_error
+      end
+   end
 end

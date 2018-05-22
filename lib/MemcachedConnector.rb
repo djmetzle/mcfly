3b58f4d86adff6b0f23c0134a0cbe3ed2ec84772
Daryl Metzler <djmetzle@ncsu.edu>
2018-05-22 13:42:00 -0700

    [minor] typo in output

--- lib/MemcachedConnector.rb
+++ lib/MemcachedConnector.rb
@@ -12,7 +12,7 @@ class MemcachedConnector
          puts "#{Time.now.to_f}: Deleted key '#{key}' from '#{@destination}'"
          return true
       rescue Memcached::NotFound
-         puts "#{Time.now.to_f}: NotFoundd key '#{key}' from '#{@destination}'"
+         puts "#{Time.now.to_f}: NotFound key '#{key}' from '#{@destination}'"
          return true
       rescue
          return false

b46410d2429c9c76ee1dbb3d87f6972bc0792714
Daryl Metzler <djmetzle@ncsu.edu>
2018-05-22 08:48:02 -0700

    Add a logging entry when delete logs are found
    This ought to get pulled out into a configurable Logging class.

--- lib/LogEngine.rb
+++ lib/LogEngine.rb
@@ -23,7 +23,7 @@ class LogEngine
          break if next_entry.nil? # needed?
          @delete_queue.push next_entry.destination, next_entry
       end
-
+      puts "#{Time.now.to_f}: Found new delete log entries!"
       return true
    end
 end

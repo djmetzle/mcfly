f96b9edeeafd8eebc3d2422f0f19f2f120d96a21
Daryl Metzler <djmetzle@ncsu.edu>
2018-05-24 14:07:24 -0700

    Fix derpy keyword arguments mistake
    For `nil` default keyword arguments, simply don't provide a value.
    Syntactically, this is very weird and breaks highlighting. But it
    works. Correctly. And doesn't throw a warning.

--- lib/Entry.rb
+++ lib/Entry.rb
@@ -5,11 +5,11 @@ class Entry
    attr_accessor :destination
    attr_accessor :mcrouter_port
 
-   def initialize(entry_str: entry_str,
-                  entry_key: entry_key,
-                  entry_pool: entry_pool,
-                  destination: destination,
-                  mcrouter_port: mcrouter_port
+   def initialize(entry_str:,
+                  entry_key:,
+                  entry_pool:,
+                  destination:,
+                  mcrouter_port:
                  )
       @entry_str = entry_str
       @entry_key = entry_key

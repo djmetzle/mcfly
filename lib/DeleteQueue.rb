eb89319f466a14193ea946b5c0984f87a5a88aa8
Daryl Metzler <daryl@ifixit.com>
2018-07-06 16:15:45 -0700

    Address Naming/UncommunicativeMethodParamName
    `ip` is a perfectly explicit parameter name. Be very verbose instead.

--- lib/DeleteQueue.rb
+++ lib/DeleteQueue.rb
@@ -7,18 +7,18 @@ class DeleteQueue
       return @hashmap.reject { |_, queue| queue.empty? }.keys
    end
 
-   def push(ip, delete)
-      @hashmap[ip] ||= []
-      @hashmap[ip].push delete
+   def push(host_address, delete)
+      @hashmap[host_address] ||= []
+      @hashmap[host_address].push delete
       return
    end
 
-   def peek(ip)
-      return @hashmap[ip].first
+   def peek(host_address)
+      return @hashmap[host_address].first
    end
 
-   def shift(ip)
-      @hashmap[ip].shift
+   def shift(host_address)
+      @hashmap[host_address].shift
       return
    end
 end
